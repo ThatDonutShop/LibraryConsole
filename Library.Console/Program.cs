5f55ea81813abdcee5293379e5c38e78b024a4ba
ThatDonutShop <ollie.foose@gmail.com>
2024-04-13 15:59:22 +1200

    bh

--- Library.Console/Program.cs
+++ Library.Console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Library.Core;
 using Spectre.Console;
+using System;
 
 AnsiConsole.MarkupLine("[teal]Weltec Library[/]");
 
@@ -9,16 +10,12 @@ do
 {
     Console.WriteLine(string.Empty);
 
+    
     var command = AnsiConsole.Prompt(new SelectionPrompt<Commands>()
         .Title(" [teal]What do you want to do[/]?")
         .PageSize(10)
         .MoreChoicesText("[grey](Move up and down to reveal more options)[/]")
-        .AddChoices(new[]
-        {
-            Commands.CreateStaffMember,
-            Commands.CreateStudent,
-            Commands.AddLibraryItem
-        })
+        .AddChoices((Commands[])Enum.GetValues(typeof(Commands)))
         .UseConverter((commandName) =>
          {
              switch (commandName)
@@ -29,6 +26,10 @@ do
                      return "Register student";
                  case Commands.AddLibraryItem:
                      return "Add item to library catalog";
+                 case Commands.RemoveLibraryItem:
+                     return "Remove item from library catalog";
+                 case Commands.ListCatalogItems:
+                     return "List items in the catalog";
                  default:
                      throw new NotImplementedException(commandName.ToString());
              }
@@ -44,6 +45,12 @@ do
             break;
         case Commands.AddLibraryItem:
             AddLibraryItem();
+            break;
+        case Commands.RemoveLibraryItem:
+            RemoveLibraryItem();
+            break;
+        case Commands.ListCatalogItems:
+            ListCatalogItems();
             break;
         default:
             throw new NotSupportedException(command.ToString());
@@ -133,11 +140,47 @@ void AddLibraryItem()
     library.Add(item);
 }
 
+void RemoveLibraryItem()
+{
+    var catalog = library.GetCatalogItems().OrderBy(c => c.Title);
+    
+    var title = AnsiConsole.Prompt(new SelectionPrompt<string>()
+        .Title("[teal]Remove library item[/]")
+        .AddChoices(catalog.Select(c => c.Title)));
+
+    library.Remove(catalog.First(c => c.Title == title));
+
+    AnsiConsole.MarkupLine("[teal]Removed the item '{0}'[/]", title);
+}
+
+void ListCatalogItems()
+{
+    var table = new Table();
+
+    table.Expand();
+    table.LeftAligned();
+    table.Border(TableBorder.Rounded);
+    table.AddColumn(new TableColumn("Title").Centered());
+    table.AddColumn(new TableColumn("Type").Centered());
+
+    foreach (var item in library
+        .GetCatalogItems()
+        .OrderBy(c => c.GetType().Name)
+        .ThenBy(c => c.Title))
+    {
+        table.AddRow(item.Title, item.GetType().Name).LeftAligned();
+    }
+
+    AnsiConsole.Write(table);
+}
+
 enum Commands
 {
     CreateStaffMember,
     CreateStudent,
-    AddLibraryItem
+    AddLibraryItem,
+    RemoveLibraryItem,
+    ListCatalogItems
 }
 
 enum ResourceTypes 
